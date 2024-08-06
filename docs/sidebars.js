57b615fe6e433cd13724736f2ddbf7eed567f759
Tomas Fernandez <46322567+TomFern@users.noreply.github.com>
2024-08-06 10:46:04 -0300

    Create Jobs YAML reference page (#143)
    * Checkpoint
    
    * Grammar check

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -169,6 +169,7 @@ const sidebars = {
         'reference/self-hosted-config',
         'reference/agent-yaml',
         'reference/dashboard-yaml',
+        'reference/jobs-yaml',
         'reference/deployment-target-yaml',
         'reference/notifications-yaml',
         'reference/pipeline-yaml',
