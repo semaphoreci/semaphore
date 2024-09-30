cd6f7df8b821df123d1c5f90c35e475b46b5726b
Tomas Fernandez <46322567+TomFern@users.noreply.github.com>
2024-09-30 19:15:59 +0100

    Create landing page (#216)
    * Move docs to root of URLs
    
    * Test features homepage
    
    * landing draft 1
    
    * Fix heights
    
    * Update landing page, add iconify icons
    
    * Rename landing page components
    
    * Update batch of images
    
    * Add batch of images
    
    * Move docs to root of URLs
    
    * Test features homepage
    
    * landing draft 1
    
    * Fix heights
    
    * Update landing page, add iconify icons
    
    * Rename landing page components
    
    * Update batch of images
    
    * Add batch of images
    
    * Update landing page

--- docs/src/components/FeatureCards/index.jsx
+++ docs/src/components/FeatureCards/index.jsx
@@ -23,31 +23,61 @@ const Features = [
   {
     name: (
       <span>
-        <Icon icon="eva:eye-outline" width="1.2em" height="1.2em" />
-        <Translate id='features.visualeditor.title'>Visual Editor</Translate>
-        </span>
+        <Icon icon="eos-icons:job" width="1.2em" height="1.2em" />
+        <Translate id='features.workflows.title'>Workflows</Translate>
+      </span>
     ),
     image: require('@site/static/img/wf1.gif'),
-    url: 'using-semaphore/workflows#workflow-editor',
+    url: 'using-semaphore/workflows',
     description: (
-      <Translate id="feature.visualeditor.description">
-        Create workflows visually. No need to learn yet another YAML syntax. Use the shell commands you know and love directly in your jobs.
+      <Translate id="features.workflows.description">
+        Workflows can build, test, release and deploy your code. Learn how to create workflows using the visual editor.
       </Translate>
     ),
   },
   {
     name: (
       <span>
-        <Icon icon="f7:logo-github" width="1.2em" height="1.2em" />
-        <Icon icon="devicon:bitbucket" width="1.2em" height="1.2em" />
-        <Translate id='features.gitvendor.title'>GitHub and BitBucket</Translate>
+        <Icon icon="carbon:batch-job" width="1.2em" height="1.2em" />
+        <Translate id='features.jobs.title'>Jobs</Translate>
+        </span>
+    ),
+    image: require('@site/static/img/job.jpg'),
+    url: 'using-semaphore/jobs',
+    description: (
+      <Translate id="feature.jobs.description">
+        Jobs are the basic unit of work. Read about jobs, blocks and pipelines.
+      </Translate>
+    ),
+  },
+  {
+  name: (
+    <span>
+      <Icon icon="carbon:batch-job" width="1.2em" height="1.2em" />
+      <Translate id='features.projects.title'>Projects</Translate>
+      </span>
+  ),
+  image: require('@site/static/img/projects.jpg'),
+  url: 'using-semaphore/projects',
+  description: (
+    <Translate id="feature.projects.description">
+      Use projects to connect your GitHub or BitBucket repositories to Semaphore.
+    </Translate>
+  ),
+  },
+  
+  {
+    name: (
+      <span>
+        <Icon icon="carbon:migrate" width="1.2em" height="1.2em" />
+        <Translate id='features.migration.title'>Migration Guides</Translate>
       </span>
     ),
-    image: require('@site/static/img/repo.jpg'),
-    url: 'using-semaphore/tests/test-reports',
+    image: require('@site/static/img/migration.jpg'),
+    url: 'getting-started/migration/overview',
     description: (
-      <Translate id="features.gitvendor.description">
-        Seamless connection with GitHub and BitBucket. Authorize Semaphore and get your projects going.
+      <Translate id="features.migration.description">
+        Check out the migration guides if you are using other CI provider like as Jenkins or CircleCI.
       </Translate>
     ),
   },
@@ -55,15 +85,29 @@ const Features = [
     name: (
       <span>
         <Icon icon="et:linegraph" width="1.2em" height="1.2em" />
-        <Translate id='features.metrics.title'>Metrics and test reports</Translate>
+        <Translate id='features.metrics.title'>Dashboards</Translate>
       </span>
     ),
-    // image: require('@site/static/img/security.jpg'),
     image: require('@site/static/img/metrics.jpg'),
-    url: 'using-semaphore/self-hosted',
+    url: 'using-semaphore/insights',
     description: (
       <Translate id="features.metrics.description">
-        Track the performance and reliability of your CI. Create custom dashboards, get detailed test reports, and identify flaky tests.
+        Create dashboards to track the performance and reliability of your CI.
+      </Translate>
+    ),
+  },
+  {
+    name: (
+      <span>
+        <Icon icon="grommet-icons:test" width="1.2em" height="1.2em" />
+        <Translate id='features.tests.title'>Test reports</Translate>
+      </span>
+    ),
+    image: require('@site/static/img/tests.jpg'),
+    url: 'using-semaphore/tests/test-reports',
+    description: (
+      <Translate id="features.tests.description">
+        Configure test reports to view all the errors and flaky tests in a single place.
       </Translate>
     ),
   },
@@ -71,15 +115,14 @@ const Features = [
     name: (
       <span>
         <Icon icon="icon-park-outline:api" width="1.2em" height="1.2em" />
-        <Translate id='features.api.title'>Make it yours with the API</Translate>
+        <Translate id='features.api.title'>Public API</Translate>
       </span>
     ),
     image: require('@site/static/img/api.jpg'),
-    url: 'https://semaphoreci.com/pricing',
+    url: 'reference/api',
     description: (
       <Translate id="features.api.description">
-        Make your custom integrations using the API. 
-        Create and manage every aspect of Semaphore with our API and CLI tool.
+        Make your custom integrations using the API. Manage Semaphore using the CLI.
       </Translate>
     ),
   },
@@ -87,31 +130,74 @@ const Features = [
     name: (
       <span>
         <Icon icon="fluent-mdl2:permissions" width="1.2em" height="1.2em" />
-        <Translate id='features.rbac.title'>RBAC and Compliance</Translate>
+        <Translate id='features.orgs.title'>Organizations</Translate>
       </span>
     ),
     image: require('@site/static/img/rbac.jpg'),
-    url: 'using-semaphore/jobs#debug-jobs',
+    url: 'using-semaphore/organizations',
     description: (
-      <Translate id="features.rbac.description">
-        Set-up roles and access levels to your engineering teams.
-        Gain regulatory compliance with audit trails for system-wide actions.
+      <Translate id="features.orgs.description">
+        Invite your team, set up roles and groups, manage secrets, and create notifications.
       </Translate>
     ),
   },
   {
     name: (
       <span>
-        <Icon icon="streamline:bug-antivirus-debugging" width="1.2em" height="1.2em" />
-        <Translate id='features.debug.title'>Debug with SSH</Translate>
+        <Icon icon="hugeicons:web-design-01" width="1.2em" height="1.2em" />
+        <Translate id='features.debug.title'>Language Guides</Translate>
       </span>
     ),
-    image: require('@site/static/img/ssh-debugging.jpg'),
-    url: 'using-semaphore/jobs#debug-jobs',
+    image: require('@site/static/img/code.jpg'),
+    url: 'using-semaphore/languages/javascript',
     description: (
       <Translate id="features.debug.description">
-        If a job is giving trouble, just jump into an interactive
-        SSH session. Debug your CI on the spot and quickly solve issues.
+        Learn how to build and test your project in your language of choice.
+      </Translate>
+    ),
+  },
+  {
+    name: (
+      <span>
+        <Icon icon="solar:server-2-broken" width="1.2em" height="1.2em" />
+        <Translate id='features.selfhosted.title'>Self-hosted Agents</Translate>
+      </span>
+    ),
+    image: require('@site/static/img/self-hosted.jpg'),
+    url: 'using-semaphore/self-hosted',
+    description: (
+      <Translate id="features.selfhosted.description">
+        Learn how to set up self-hosted agents to run jobs in your own infrastructure.
+      </Translate>
+    ),
+  },
+  {
+    name: (
+      <span>
+        <Icon icon="ri:speed-mini-line" width="1.2em" height="1.2em" />
+        <Translate id='features.optimization.title'>Optimization Guide</Translate>
+      </span>
+    ),
+    image: require('@site/static/img/optimization.jpg'),
+    url: 'using-semaphore/optimization/cache',
+    description: (
+      <Translate id="features.optimization.description">
+        Learn how to speed up workflows using the cache and container registry.
+      </Translate>
+    ),
+  },
+  {
+    name: (
+      <span>
+        <Icon icon="humbleicons:arrow-side-join-main" width="1.2em" height="1.2em" />
+        <Translate id='features.monorepo.title'>Monorepo Workflows</Translate>
+      </span>
+    ),
+    image: require('@site/static/img/monorepo.jpg'),
+    url: 'using-semaphore/monorepo',
+    description: (
+      <Translate id="features.monorepo.description">
+        Read how to use change detection to speed up monorepo workflows.
       </Translate>
     ),
   },
@@ -121,13 +207,13 @@ const Features = [
 function FeatureCard({name, image, url, urlTS, description}) {
   return (
     <div className="col col--4 margin-bottom--lg">
-      <div className={clsx('card')} style={{height: "500px"}}>
+      <div className={clsx('card')} style={{height: "460px"}}>
         <div className={clsx('card__image')}>
           <Link to={url}>
             <Image img={image} alt={`${name}'s image`} />
           </Link>
         </div>
-        <div className="card__body">
+        <div className="card__body" >
           <Heading as="h3">{name}</Heading>
           <p>{description}</p>
         </div>
