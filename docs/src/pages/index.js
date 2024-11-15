307de388efd7497cf5350ebdf2f9450b86622ef8
tomas f <46322567+TomFern@users.noreply.github.com>
2024-11-15 21:54:06 +0000

    Customize homepage colors and banner

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -2,26 +2,23 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-//import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { useColorMode } from '@docusaurus/theme-common';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
-// import LinkCard from '@site/src/components/LinkCard';
-// import FlashyCard from '@site/src/components/FlashyCard';
-
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   const githubStarsUrl=`https://ghbtns.com/github-btn.html?user=${siteConfig.organizationName}&repo=${siteConfig.projectName}&type=star&count=true&size=large`;
+  const { colorMode } = useColorMode();
  
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header style={{"background": "rgba(0,0,0,0)"}} className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+        <Heading as="h1" className={`hero__title ${colorMode}`}>
+          <span>{siteConfig.title}</span>
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className={`hero__subtitle ${colorMode}`} >{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
